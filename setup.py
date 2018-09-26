39b75fc154c331920d272ec6f73d3797817dcb10
Richard O'Dwyer <richard@richard.do>
2018-09-26 11:41:33 +0100

    Back to development: 1.7.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name=NAME,
-    version='1.7.0',
+    version='1.7.1.dev0',
     description=DESCRIPTION,
     long_description=long_description,
     author=AUTHOR,
